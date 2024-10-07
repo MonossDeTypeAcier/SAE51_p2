0510d36bc98d99b12842c89b198ea2b8182ed13f
LeGrandMonoss <73824743+LeGrandMonoss@users.noreply.github.com>
2024-10-08 00:06:48 +0200

    Delete logAlloy/run.sh

--- logAlloy/run.sh
+++
@@ -1,6 +0,0 @@
-docker run \
-	-v /root/SAE51_p2/logAlloy/alloy/:/etc/alloy/ \
-	-p 12345:12345 \
-	grafana/alloy:latest \
-		run --server.http.listen-addr=0.0.0.0:12345 --storage.path=/var/lib/alloy/data \
-		/etc/alloy/config.alloy
